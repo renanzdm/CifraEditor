c003e784b165a9935af20d6de35f18f7fcb978b5
renanzdm <renanzdm@gmail.com>
2023-11-01 10:12:27 -0300

    get ip address connected wiffi

--- app/src/main/java/com/renan/cifraeditor/presenter/soundcontrol/SoundControlPage.kt
+++ app/src/main/java/com/renan/cifraeditor/presenter/soundcontrol/SoundControlPage.kt
@@ -2,6 +2,7 @@ package com.renan.cifraeditor.presenter.soundcontrol
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.LinkProperties
 import android.net.wifi.ScanResult
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -58,20 +59,11 @@ fun SoundControlPage(
     val uiState = soundControlViewModel.state.collectAsStateWithLifecycle()
     val permissionLauncher =
         rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestMultiplePermissions()) {
-            soundControlViewModel.startScan()
         }
     val listPermissions = arrayOf(
         Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
     )
-    if (uiState.value.openDialog) {
-        DialogAvailableNetworks(
-            closeDialog = { soundControlViewModel.closeDialogResults() },
-            listResults = uiState.value.availableNetwork,
-            loading = uiState.value.loading
-        )
-    }
     LifecycleStartEffect(lifecycleOwner = LocalLifecycleOwner.current) {
-        soundControlViewModel.registerBroadcast(context = context)
         onStopOrDispose { }
     }
     Scaffold(topBar = { AppTopBar(title = "Controle do Som") }) { paddingValues ->
@@ -81,7 +73,12 @@ fun SoundControlPage(
                 .verticalScroll(scrollState)
         ) {
             Button(onClick = {
-                soundControlViewModel.getIpAddressConnectedWifi(context)
+                val currentNetwork =
+                    soundControlViewModel.getConnectivityManager(context).activeNetwork
+               val properties:LinkProperties? =  soundControlViewModel.getConnectivityManager(context)
+                    .getLinkProperties(currentNetwork)
+                println(properties?.linkAddresses)
+
 //                val permissionCheckResultFineLocation = ContextCompat.checkSelfPermission(
 //                    context, Manifest.permission.ACCESS_FINE_LOCATION
 //                )
@@ -128,7 +125,7 @@ fun DialogAvailableNetworks(
                     }
                 }
                 items(items = listResults) {
-                    Row(modifier = Modifier.padding(vertical = 8.dp)){
+                    Row(modifier = Modifier.padding(vertical = 8.dp)) {
                         IconWifiFrequency(it.level)
                         Spacer(modifier = Modifier.width(12.dp))
                         Text(text = it.SSID)
@@ -140,7 +137,7 @@ fun DialogAvailableNetworks(
 }
 
 @Composable
-fun IconWifiFrequency(level:Int) {
+fun IconWifiFrequency(level: Int) {
     val size = 50.dp
     val pathFirstLine by remember {
         mutableStateOf(Path())
@@ -151,48 +148,52 @@ fun IconWifiFrequency(level:Int) {
     val pathThirdLine by remember {
         mutableStateOf(Path())
     }
-        Canvas(
-            modifier = Modifier.padding(end = 20.dp)
-        ) {
-            ///firstLine
-            pathFirstLine.moveTo(0f, (size.value * 0.5).toFloat())
-            pathFirstLine.lineTo(0f, (size.value * 0.5).toFloat())
-            pathFirstLine.quadraticBezierTo(
-                (size.value * 0.5).toFloat(), 4f, size.value, (size.value * 0.5).toFloat()
+    Canvas(
+        modifier = Modifier.padding(end = 20.dp)
+    ) {
+        ///firstLine
+        pathFirstLine.moveTo(0f, (size.value * 0.5).toFloat())
+        pathFirstLine.lineTo(0f, (size.value * 0.5).toFloat())
+        pathFirstLine.quadraticBezierTo(
+            (size.value * 0.5).toFloat(), 4f, size.value, (size.value * 0.5).toFloat()
+        )
+        ///secondLine
+        pathSecondLine.moveTo((size.value * 0.2).toFloat(), (size.value * 0.8).toFloat())
+        pathSecondLine.lineTo((size.value * 0.2).toFloat(), (size.value * 0.8).toFloat())
+        pathSecondLine.quadraticBezierTo(
+            ((size.value) * 0.5).toFloat(),
+            30f,
+            (size.value * 0.8).toFloat(),
+            (size.value * 0.8).toFloat()
+        )
+        //thirdLine
+        pathThirdLine.moveTo((size.value * 0.4).toFloat(), size.value)
+        pathThirdLine.lineTo((size.value * 0.4).toFloat(), size.value)
+        pathThirdLine.quadraticBezierTo(
+            (size.value * 0.5).toFloat(),
+            45f,
+            (size.value * 0.6).toFloat(),
+            size.value
+        )
+        drawPath(
+            pathFirstLine,
+            color = if (level >= -40) Color.White else Color.White.copy(alpha = 0.6f),
+            style = Stroke(
+                width = 5f, cap = StrokeCap.Round
             )
-            ///secondLine
-            pathSecondLine.moveTo((size.value * 0.2).toFloat(), (size.value * 0.8).toFloat())
-            pathSecondLine.lineTo((size.value * 0.2).toFloat(), (size.value * 0.8).toFloat())
-            pathSecondLine.quadraticBezierTo(
-                ((size.value) * 0.5).toFloat(),
-                30f,
-                (size.value * 0.8).toFloat(),
-                (size.value * 0.8).toFloat()
+        )
+        drawPath(
+            pathSecondLine,
+            color = if (level >= -70) Color.White else Color.White.copy(alpha = 0.6f),
+            style = Stroke(
+                width = 5f, cap = StrokeCap.Round
             )
-            //thirdLine
-            pathThirdLine.moveTo((size.value * 0.4).toFloat(), size.value)
-            pathThirdLine.lineTo((size.value * 0.4).toFloat(), size.value)
-            pathThirdLine.quadraticBezierTo(
-                (size.value * 0.5).toFloat(),
-                45f,
-                (size.value * 0.6).toFloat(),
-                size.value
+        )
+        drawPath(
+            pathThirdLine, color = Color.White, style = Stroke(
+                width = 5f, cap = StrokeCap.Round
             )
-            drawPath(
-                pathFirstLine, color = if (level >= -40) Color.White else Color.White.copy(alpha = 0.6f), style = Stroke(
-                    width = 5f, cap = StrokeCap.Round
-                )
-            )
-            drawPath(
-                pathSecondLine, color =if (level >= -70) Color.White else Color.White.copy(alpha = 0.6f), style = Stroke(
-                    width = 5f, cap = StrokeCap.Round
-                )
-            )
-            drawPath(
-                pathThirdLine, color = Color.White, style = Stroke(
-                    width = 5f, cap = StrokeCap.Round
-                )
-            )
-        }
+        )
+    }
 }
 
--- app/src/main/java/com/renan/cifraeditor/presenter/soundcontrol/SoundControlViewModel.kt
+++ app/src/main/java/com/renan/cifraeditor/presenter/soundcontrol/SoundControlViewModel.kt
@@ -1,22 +1,17 @@
 package com.renan.cifraeditor.presenter.soundcontrol
 
-import android.annotation.SuppressLint
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.net.wifi.WifiManager
-import android.text.format.Formatter
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import java.net.InetAddress
 import javax.inject.Inject
 
 
@@ -25,50 +20,58 @@ class SoundControlViewModel @Inject constructor() : ViewModel() {
     private val _state = MutableStateFlow(SoundControlState())
     var state = _state.asStateFlow()
     private lateinit var wifiManager: WifiManager
-    private val wifiScanReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val success: Boolean = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
-            if (success) {
-                scanSuccess()
-            } else {
-                scanFailure()
-            }
-        }
-    }
+//    private val wifiScanReceiver = object : BroadcastReceiver() {
+//        override fun onReceive(context: Context, intent: Intent) {
+//            val success: Boolean = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+//            if (success) {
+//                scanSuccess()
+//            } else {
+//                scanFailure()
+//            }
+//        }
+//    }
 
-    fun registerBroadcast(context: Context) {
-        val intentFilter = IntentFilter()
-        wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-        context.registerReceiver(wifiScanReceiver, intentFilter)
 
-    }
 
-    fun startScan() {
-        _state.update { it.copy(openDialog = true, loading = true) }
-        wifiManager.startScan()
-    }
 
-    @SuppressLint("MissingPermission")
-    private fun scanSuccess() {
-        _state.update { it.copy(loading = false) }
-        val results = wifiManager.scanResults
-        _state.update { it.copy(availableNetwork = results) }
-    }
+    fun getConnectivityManager(context: Context) =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-    private fun scanFailure() {
 
-    }
+//    fun registerBroadcast(context: Context) {
+//        val intentFilter = IntentFilter()
+//        wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+//        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+//        context.registerReceiver(wifiScanReceiver, intentFilter)
+//
+//    }
+
+//    fun startScan() {
+//        _state.update { it.copy(openDialog = true, loading = true) }
+//        wifiManager.startScan()
+//    }
+
+//    @SuppressLint("MissingPermission")
+//    private fun scanSuccess() {
+//        _state.update { it.copy(loading = false) }
+//        val results = wifiManager.scanResults
+//        _state.update { it.copy(availableNetwork = results) }
+//    }
+//
+//    private fun scanFailure() {
+//
+//    }
 
     fun closeDialogResults() {
         _state.update { it.copy(openDialog = false) }
     }
 
-    fun getIpAddressConnectedWifi(context:Context){
-        viewModelScope.launch (Dispatchers.IO){
-        }
-
+    private fun getNetworkRequest(): NetworkRequest {
+        return NetworkRequest.Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)    //here!!
+            .build()
     }
+
 
 
 }
