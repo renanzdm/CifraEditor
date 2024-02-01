f29fd1705beee1dc768f79bdbc8d2246ae75a76b
renan.mirand <renan.miranda@agrotis.com>
2024-02-01 15:01:11 -0300

    feat (add gcp environment)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.renan.cifraeditor"
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
