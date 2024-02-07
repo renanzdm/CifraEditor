d56f4a3d00b37abe8aa7052207cc36c052afdea9
renan.mirand <renan.miranda@agrotis.com>
2024-02-07 13:27:28 -0300

    update name version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.renan.cifraeditor"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
